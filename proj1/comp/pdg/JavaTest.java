0ae7e3de8a3346cc111b530230953f73958c73a8
Sapphirus4 <up201106586@fe.up.pt>
2016-04-28 20:43:46 +0100

    Created new branch

--- proj1/comp/pdg/JavaTest.java
+++ proj1/comp/pdg/JavaTest.java
@@ -2,27 +2,25 @@ package comp.pdg;
 
 import comp.dot.DotGraph;
 
-public class JavaTest
-{
-	public static void main(String[] args)
-	{
-		DotGraph g = new DotGraph("Example");
-		g.setEdgeColor("blue");
-		g.setEdgeStyle("dotted");
-		g.setDpi(15);
-		g.connectDirected("A", "B", "X");
-		g.setVertexColor("A", "red");
-		g.setVertexColor("B", "yellow");
-		g.setVertexLabel("A", "WHAT");
-		g.setVertexShape("A", "box");
-		g.setVertexShape("B", "box");
-		g.resetEdgeColor();
-		g.connectDirected("B", "C", "Y");
-		g.resetEdgeStyle();
-		g.setEdgeWidth(5.0f);
-		g.setEdgeColor("green");
-		g.connectDirected("B", "D", "Z");
-		g.generateDotFile("myGraph.dot");
-		g.outputGraph("pdf", "dot");
-	}
+public class JavaTest {
+    public static void main(String[] args) {
+        DotGraph g = new DotGraph("Example");
+        g.setEdgeColor("blue");
+        g.setEdgeStyle("dotted");
+        g.setDpi(15);
+        g.connectDirected("A", "B", "X");
+        g.setVertexColor("A", "red");
+        g.setVertexColor("B", "yellow");
+        g.setVertexLabel("A", "WHAT");
+        g.setVertexShape("A", "box");
+        g.setVertexShape("B", "box");
+        g.resetEdgeColor();
+        g.connectDirected("B", "C", "Y");
+        g.resetEdgeStyle();
+        g.setEdgeWidth(5.0f);
+        g.setEdgeColor("green");
+        g.connectDirected("B", "D", "Z");
+        g.generateDotFile("myGraph.dot");
+        g.outputGraph("pdf", "dot");
+    }
 }
